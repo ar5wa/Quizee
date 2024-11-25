28a0102e9d1905fc87efd5911afbfc6076f44b8e
ar5wa <arwarashedalkh@outlook.sa>
2024-11-25 12:52:41 +0300

    Q

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Dynamic;
+
+
+    /*class student
+    {
+     public string name {get; set;}
+     public int age {get; set;}
+     public string grade {get; set;}
+
+    
+    public void DisplayInfo()
+    {
+     Console.WriteLine("name : " +name);
+     Console.WriteLine("age : " +age);
+     Console.WriteLine("garde : " +grade);
+     
+     
+    }
+    }
+    class Program
+    {static void Main (string[]arges)
+    {
+       student student1 = new student {name = " arwa ",age =  18 , grade ="diploma"};
+       student student2 =  new student {name = "lama " ,age = 27, grade= "master"};
+       student student3 = new student { name = "rawan" ,age =29, grade= "graduted "};
+
+      student1.DisplayInfo();
+      student2.DisplayInfo();
+      student3.DisplayInfo();
+    }
+    
+}/*/
+
+/*class Program
+{
+ static void Main (string []args)
+ {
+    string name = "jhon";
+    int age = 30;
+    double hourlywage = 20.5;
+     
+     Console.WriteLine("hi ,my name is "+name + " i am "+age +" old and earn"+ hourlywage+ " per hour." );
+ }
+}*/
+class Program
+{
+    static void Main (string[]args)
+    {
+       Console.WriteLine("enter number ");
+       Console.ReadLine ();
+       
+    }
+}
